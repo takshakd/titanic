7cb63d34a77acfb1212abd8989119acb06e7518f
Takshak Desai <takshak@live.com>
2013-11-07 23:18:39 -0500

    Fixed bugs in computing percentages of survivors.

--- readTrain.py
+++ readTrain.py
@@ -1,6 +1,8 @@
 import csv as csv
 import numpy as np
 
+print "------------------------------------"
+
 fp = csv.reader(open('train.csv', 'rb'))
 header = fp.next()
 
@@ -10,22 +12,21 @@ for row in fp:
 
 data = np.array(data)
 
-Npassengers = np.size(data[0::,0].astype(np.float))
-Nsurvivors  = np.sum(data[0::,0].astype(np.float))
-percentage = Nsurvivors/Npassengers;
+Npassengers = np.size(data[0::, 0])
+Nsurvivors  = np.sum((data[0::, 1]=="1").astype(np.float))
+percentage = 100*Nsurvivors/Npassengers;
+print "Percentage survivors = %s" % percentage
 
-womenIdx = data[0::,4] == "female"
-menIdx   = data[0::,4] == "male"
+women = (data[0::,4] == "female")
+men   = (data[0::,4] == "male")
 
-womenOnboard = data[womenIdx, 0].astype(np.float)
-menOnboard   = data[menIdx, 0].astype(np.float)
+womenSurvivors = (data[women, 1] == "1").astype(np.float)
+menSurvivors   = (data[men, 1] == "1").astype(np.float)
 
-womenSurvivors = data[womenIdx, 1] == "1"
-menSurvivors   = data[menIdx, 1] == "1"
-
-percentageWomenSurvivors = np.sum(womenOnboard) / np.size(womenOnboard)
-percentageMenSurvivors   = np.sum(menOnboard) / np.size(menOnboard)
+percentageWomenSurvivors = 100 * np.sum(womenSurvivors) / np.sum(women.astype(np.float))
+percentageMenSurvivors   = 100 * np.sum(menSurvivors) / np.sum(men.astype(np.float))
 
 print 'Percentage of women survivors = %s' % percentageWomenSurvivors
 print 'Percentage of men survivors   = %s' % percentageMenSurvivors
 
+print "------------------------------------"
