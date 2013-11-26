6927c1abb0ab9aefd225b885a89f337885edbb32
takshakd <takshak@live.com>
2013-11-26 08:03:47 -0500

    Function-ify the act of predicting.

---
+++ predict.py
@@ -0,0 +1,5 @@
+def predict(featureVector):
+    if featureVector[3] == "male":
+        return 0
+    else:
+        return 1
--- writePredictions.py
+++ writePredictions.py
@@ -1,6 +1,8 @@
 import csv as csv
 import numpy as np
 
+from predict import predict
+
 print "Writing predictions . . ."
 
 modelfile = csv.writer(open("predictions.csv", "wb"))
@@ -9,9 +11,6 @@ testfile = csv.reader(open("test.csv", "rb"))
 testfile.next()
 
 for row in testfile:
-    if row[3] == "female":
-        row.insert(0, "lives")
-        modelfile.writerow(row)
-    else:
-        row.insert(0, "dies")
-        modelfile.writerow(row)
+    prediction = predict(row)
+    row.insert(0, prediction)
+    modelfile.writerow(row)
